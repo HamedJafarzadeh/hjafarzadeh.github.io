0a4b92fb5aa91097d82b11003df12e03a76e5191
Hamed <hjafarzadeh511@gmail.com>
2020-10-27 09:55:37 +0000

    Add index to blog

---
+++ _nuxt/66d1fb5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{235:function(e,n,t){"use strict";t.r(n);var c={name:"IMPAQT"},r=t(61),component=Object(r.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"dd58cf64",null);n.default=component.exports}}]);
---
+++ _nuxt/static/1603792401/IMPAQT/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/IMPAQT", {data:[{}],fetch:[],mutations:void 0});
