e6f48378642f8361d7f865e750286bd378f9d3aa
Hamed <hjafarzadeh511@gmail.com>
2021-01-12 07:52:54 +0000

    Update award for IMPAQT

---
+++ _nuxt/static/1610437958/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FIMPAQT","\u002Fblog","\u002Fblog\u002FSTM32","\u002Fblog\u002FAltium","\u002Fblog\u002Fielts7","\u002Fblog\u002Fsideprojects","\u002Fblog\u002Fketo","\u002Fblog\u002Fvisualodometry","\u002Fblog\u002FIsolatedBatteryVoltageMeasurement","\u002Fblog\u002FROS-Bag-D435"]})
